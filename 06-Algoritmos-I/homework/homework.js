a7e1c53b917e4a054c3df2e8f223bd0db7ce0b2f
agustinteran <agustintern@gmail.com>
2022-05-03 20:56:44 -0300

    Algoritmos I

--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -6,7 +6,14 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var acum = [1]
+  var nro = num
+  var i = 2
+  while(nro !== 1){
+    if (nro % i == 0) {acum.push(i) ; nro = nro / i; i = 1};
+    i++
+  }
+  return acum
 }
 
 function bubbleSort(array) {
@@ -14,7 +21,17 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  
+  var a = 0
+  var b = 0
+  for (var i = 0; i < array.length; i++) {
+         if(array[i] > array[i + 1]){b = array[i + 1]; a = array[i]; array[i] = b ; array [i + 1] = a}
 
+  }
+  for (var i = 0; i < array.length; i++) {
+     if (array[i] > array[i + 1]) {bubbleSort(array)}
+  }
+  return array
 }
 
 
@@ -23,7 +40,17 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var i = 1
+  var a = 0
+  var b = 0
+  for (var i = 1; i < array.length; i++) {
+      var j = i - 1
+      while(j >= 0){
+           if (array[i] < array[j]) {b = array[i] ; a = array[j]; array[i] = a ; array[j] = b; i--};
+           j--
+      }
+    }
+    return array
 }
 
 
@@ -32,7 +59,15 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+   var a = 0
+   var b = 0
+    for (var i = 0; i < array.length; i++) {
+            for (var j = i + 1; j < array.length; j++) {
+                    if (array[i] > array[j]) {a = array[i]; b = array[j]; array[i] = b; array[j] = a}
+                    
+            }
+    }
+      return array
 }
 
 
